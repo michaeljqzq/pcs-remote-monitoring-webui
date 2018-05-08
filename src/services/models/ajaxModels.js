f481505532fa96ce811639e8c3cb35fd7444597e
mechaffin <a-machaf>
2018-05-08 12:01:51 -0700

    Better parsing for exceptions

--- src/services/models/ajaxModels.js
+++ src/services/models/ajaxModels.js
@@ -3,6 +3,7 @@
 import Config from 'app.config';
 
 // Error response abstractions
+const parseApplicationMessage = Symbol('parseApplicationMessage');
 
 export class AjaxError {
 
@@ -11,10 +12,34 @@ export class AjaxError {
   constructor(ajaxError) {
     this.ajaxError = ajaxError;
     const resp = ajaxError.response || {};
-    this.errorMessage = resp.ExceptionMessage || resp.Message || resp.Error || ajaxError.message || `An unknown error occurred`;
+
+    // In Java, the ExceptionMessage contains a generic message and info about the request.
+    // But the InnerExceptionMessage message looks to contain information about the actual error.
+    this.errorMessage = resp.InnerExceptionMessage || resp.ExceptionMessage || resp.Message || resp.Error || ajaxError.message || `An unknown error occurred`;
     this.status = ajaxError.status;
+
     // Log all errors in the console
     console.error(ajaxError);
+
+    // For general application errors, figure out a more detailed message if possible.
+    // In dotNet, the ExceptionMessage may contain JSON that can be further parsed.
+    if (this.status >= 500 && this.errorMessage) {
+      this[parseApplicationMessage](((JSON.parse(this.errorMessage) || {}).Message) || this.errorMessage);
+    }
+  }
+
+  /**
+   * Parse the message for known error codes
+   */
+  [parseApplicationMessage](message) {
+    if (message) {
+      this.errorMessage = message;
+
+      // TODO: Add other error codes that we care about
+      if (message.includes('ThrottlingMaxActiveJobCountExceeded')) {
+        this.errorCode = 'maxJobCountExceeded';
+      }
+    }
   }
 
   /**
@@ -29,10 +54,14 @@ export class AjaxError {
       return 'errorCode.notAuthorized';
     } else if (this.status === 404) { // Endpoint not found
       return 'errorCode.notFound';
+    } else if (this.status === 429) { // Max Active Job Count Exceeded
+      return 'errorCode.maxJobCountExceeded';
     } else if (this.status >= 300 && this.status < 400) { // Redirection
       return 'errorCode.redirection';
     } else if (Config.retryableStatusCodes.has(this.status)) {
       return 'errorCode.retryFailure';
+    } else if (this.status >= 500 && this.errorCode) {
+      return `errorCode.${this.errorCode}`;
     }
     return 'errorCode.unknown';
   }
